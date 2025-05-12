3eccc800d76a1a8d2d6ae65555b423b6fb961ef5
CG <96789431+CeeGitau@users.noreply.github.com>
2025-05-12 12:36:34 +0300

    Added game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+
 function App() {
-
-
   return (
     <main>
       <div id="game-container">
@@ -10,7 +10,7 @@ function App() {
           <Player initialName="Player 2" symbol="O" />
         </ol>
 
-        GAME BOARD
+        <GameBoard />
       </div>
 
       LOG
